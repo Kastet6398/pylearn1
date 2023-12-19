6f41c9637072b80a5984877dbf1cce642e3d7c68
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-12-19 19:24:53 +0200

    Update middleware.py

--- main/middleware.py
+++ main/middleware.py
@@ -1,5 +1,6 @@
 from .models import RateLimit
 from datetime import datetime, timedelta, timezone
+from django.db import transaction
 from django.core.exceptions import PermissionDenied
 
 class RateLimitMiddleware:
@@ -10,21 +11,23 @@ class RateLimitMiddleware:
         ip_address = self.get_client_ip(request)
         allowed_requests = 10
         time_window = 60
-        rate_limit, created = RateLimit.objects.get_or_create(ip_address=ip_address)
 
-        # Use timezone-aware datetime objects
-        current_time = datetime.utcnow().replace(tzinfo=timezone.utc)
+        with transaction.atomic():
+            rate_limit, created = RateLimit.objects.select_for_update().get_or_create(ip_address=ip_address)
 
-        if (current_time - rate_limit.timestamp).total_seconds() >= time_window:
-            rate_limit.connection_number = 0
-            rate_limit.timestamp = current_time  # Update the timestamp
+            # Use timezone-aware datetime objects
+            current_time = datetime.utcnow().replace(tzinfo=timezone.utc)
+
+            if (current_time - rate_limit.timestamp).total_seconds() >= time_window:
+                rate_limit.connection_number = 0
+                rate_limit.timestamp = current_time  # Update the timestamp
+                rate_limit.save()
+
+            if rate_limit.connection_number >= allowed_requests:
+                return PermissionDenied('Too many requests')
+
+            rate_limit.connection_number += 1
             rate_limit.save()
-
-        if rate_limit.connection_number >= allowed_requests:
-            raise PermissionDenied('Too many requests')
-
-        rate_limit.connection_number += 1
-        rate_limit.save()
 
         response = self.get_response(request)
         return response
