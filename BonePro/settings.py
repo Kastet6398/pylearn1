cefb3ac0437015116cfd21c8fc84596d47dd9ee9
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-12-19 11:06:21 +0200

    Update settings.py

--- BonePro/settings.py
+++ BonePro/settings.py
@@ -13,7 +13,7 @@ import os
 from pathlib import Path
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
-
+RATE_LIMIT = '10/m'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
