41d4c2efc257ff03b4b13f7aab6f6218a5375aed
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-12-19 13:16:07 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+sudo apt upgrade
+sudo apt update
+sudo apt install python3-venv
+pip install venv
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+python3 manage.py collectstatic --noinput
+python3 manage.py makemigrations
+python3 manage.py migrate
