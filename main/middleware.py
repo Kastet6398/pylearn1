0275931422a2d41762c7c5b7479efe306aaf4f9c
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-12-19 20:31:01 +0200

    Update middleware.py

--- main/middleware.py
+++ main/middleware.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta, timezone
 from django.db import transaction
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponseForbidden
+from django.conf import settings
 
 class RateLimitMiddleware:
     def __init__(self, get_response):
@@ -10,25 +11,26 @@ class RateLimitMiddleware:
 
     def __call__(self, request):
         ip_address = self.get_client_ip(request)
-        allowed_requests = 20
-        time_window = 60
-
-        with transaction.atomic():
-            rate_limit, created = RateLimit.objects.select_for_update().get_or_create(ip_address=ip_address)
-
-            # Use timezone-aware datetime objects
-            current_time = datetime.utcnow().replace(tzinfo=timezone.utc)
-
-            if (current_time - rate_limit.timestamp).total_seconds() >= time_window:
-                rate_limit.connection_number = 0
-                rate_limit.timestamp = current_time  # Update the timestamp
+        is_static_request = request.path.startswith(settings.STATIC_URL)
+        if not is_static_request:
+            ip_address = self.get_client_ip(request)
+            allowed_requests = 20
+            time_window = 60
+    
+            with transaction.atomic():
+                rate_limit, created = RateLimit.objects.select_for_update().get_or_create(ip_address=ip_address)
+                current_time = datetime.utcnow().replace(tzinfo=timezone.utc)
+    
+                if (current_time - rate_limit.timestamp).total_seconds() >= time_window:
+                    rate_limit.connection_number = 0
+                    rate_limit.timestamp = current_time
+                    rate_limit.save()
+    
+                if rate_limit.connection_number >= allowed_requests:
+                    return HttpResponseForbidden('Too many requests. Please try again later.')
+    
+                rate_limit.connection_number += 1
                 rate_limit.save()
-
-            if rate_limit.connection_number >= allowed_requests:
-                return HttpResponseForbidden('Too many requests. Please try again later.')
-
-            rate_limit.connection_number += 1
-            rate_limit.save()
 
         response = self.get_response(request)
         return response
