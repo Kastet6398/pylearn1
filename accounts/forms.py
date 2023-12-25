7458164ff1ab33b2ea3fa67cefb1b991f060aa01
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-12-25 14:25:17 +0200

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -13,8 +13,8 @@ class RegisterUserForm(UserCreationForm):
     email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
     first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label="Password")
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}), label="Confirm password")
 
     class Meta:
         model = User
