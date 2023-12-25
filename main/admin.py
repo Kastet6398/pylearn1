84868c1c90f877ac890043f781ec98df65829f64
Konstantin <132978012+Kastet6398@users.noreply.github.com>
2023-12-25 07:55:26 +0200

    Update admin.py

--- main/admin.py
+++ main/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Course, Theme, Test, Question, Answer
 
-admin.site.register(Course)
-admin.site.register(Theme)
-admin.site.register(Question)
-admin.site.register(Answer)
-admin.site.register(Test)
+class CourseAdmin(admin.ModelAdmin):
+    list_per_page = 15
+
+class ThemeAdmin(admin.ModelAdmin):
+    list_per_page = 15
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_per_page = 15
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_per_page = 15
+
+class TestAdmin(admin.ModelAdmin):
+    list_per_page = 15
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Theme, ThemeAdmin)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
+admin.site.register(Test, TestAdmin)
