131b017e9a3d5eaa19c51835fb733be612faa382
Kastet6398 <konstantin.nezhalsky@gmail.com>
2023-12-28 11:31:51 +0200

    r

--- static/admin/js/toggleInvitedUsersField.js
+++ static/admin/js/toggleInvitedUsersField.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-document.getElementById("id_choose_who_can_view_the_course").change(function(){
-        if (document.getElementById("id_choose_who_can_view_the_course").is(':checked')) {
-            document.getElementByClassName("field-invited_users").style.display = ""
+document.getElementById('view').contentWindow.document.body.getElementById("id_choose_who_can_view_the_course").change(function(){
+        if (document.getElementById('view').contentWindow.document.body.getElementById("id_choose_who_can_view_the_course").is(':checked')) {
+            document.getElementById('view').contentWindow.document.body.getElementByClassName("field-invited_users").style.display = ""
         } else {
-            document.getElementByClassName("field-invited_users").style.display = "none"
+            document.getElementById('view').contentWindow.document.body.getElementByClassName("field-invited_users").style.display = "none"
         }
     });
 });
