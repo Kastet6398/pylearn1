713681275b3892f478151899ab3cd3c495a9db43
Kastet6398 <konstantin.nezhalsky@gmail.com>
2023-12-28 11:48:56 +0200

    4

--- static/admin/js/toggleInvitedUsersField.js
+++ static/admin/js/toggleInvitedUsersField.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
-document.getElementById('view').contentWindow.document.body.getElementById("id_choose_who_can_view_the_course").change(function(){
-        if (document.getElementById('view').contentWindow.document.body.getElementById("id_choose_who_can_view_the_course").is(':checked')) {
-            document.getElementById('view').contentWindow.document.body.getElementByClassName("field-invited_users").style.display = ""
+    document.getElementById('view').contentWindow.document.getElementById("id_choose_who_can_view_the_course").onchange=function(){
+        if (document.getElementById('view').contentWindow.document.getElementById("id_choose_who_can_view_the_course").checked) {
+            document.getElementById('view').contentWindow.document.getElementsByClassName("field-invited_users")[0].style.display = "block";
         } else {
-            document.getElementById('view').contentWindow.document.body.getElementByClassName("field-invited_users").style.display = "none"
+            document.getElementById('view').contentWindow.document.getElementsByClassName("field-invited_users")[0].style.display = "none";
         }
-    });
+    };
 });
