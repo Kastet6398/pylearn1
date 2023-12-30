c97b896e2749df9ee2f4a48977bcdbcbcf34b9e9
Kastet6398 <konstantin.nezhalsky@gmail.com>
2023-12-30 09:28:46 +0200

    files

---
+++ static/winter/script.js
@@ -0,0 +1,18 @@
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+function createSnowflake() {
+  if (!document.hidden) {
+    const snowflake = document.createElement('div');
+    snowflake.className = 'snowflake';
+    snowflake.innerHTML = '❄️';
+    snowflake.style.left = `${getRandomNumber(0, window.innerWidth)}px`;
+    document.body.appendChild(snowflake);
+
+    snowflake.addEventListener('animationiteration', () => {
+      snowflake.remove();
+    });
+  }
+}
+createSnowflake();
+setInterval(createSnowflake, 1000);
