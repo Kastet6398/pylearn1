5f7de39a385854ee8e3bb7650b115e85aad0ba6b
Konstantin Nezhalsky <132978012+Kastet6398@users.noreply.github.com>
2024-02-17 10:49:57 +0200

    Update urls.py

--- BonePro/urls.py
+++ BonePro/urls.py
@@ -25,6 +25,6 @@ from django.contrib import admin
 urlpatterns = [
     # path('admin/', admin.site.urls),
     # path('', include(main.urls)),
-    path('', main.urls.redirectToRender),
+    path('', main.urls.redirect_to_render),
     # path('accounts/', include(accounts.urls)),
 ]
